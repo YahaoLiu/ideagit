b513ca0d35f2985a50a03b72ffe61e5b1786edb0
Liu <1558572845@qq.com>
2020-07-08 08:43:07 +0800

    版本2

--- hellogit/src/com/itheima/git/Hello.java
+++ hellogit/src/com/itheima/git/Hello.java
@@ -3,5 +3,6 @@ package com.itheima.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("qedqwadasd");
+        System.out.println("qedqwadasd version2");
     }
 }
